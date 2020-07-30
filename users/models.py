91a57f17edd58e5fbd109f7c903b11670507ddda
Tomasz W <facebaster@gmail.com>
2020-07-30 10:33:23 +0200

    Django blog first commit

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # 1-to-1 relation, 1 user = 1 profile,
+    # CASCADE - deleting user = deleting profile
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    opis = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} - Profil'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
